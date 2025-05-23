08ca0df653f45b48044acf4a7b8f5c248e5fb3cc
Justice Adam <justiceadam673@gmail.com>
2025-05-23 23:41:50 +0100

    sdhsdsdj

--- src/FireBaseConfig.js
+++ src/FireBaseConfig.js
@@ -1,9 +1,33 @@
-// src/firebase.js
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"; // ✅ Import Auth
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore"; // ✅
+// // src/firebase.js
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth"; // ✅ Import Auth
+// import { getAnalytics } from "firebase/analytics";
+// import { getFirestore } from "firebase/firestore"; // ✅
 
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: "fwan-98510.firebaseapp.com",
+//   projectId: "fwan-98510",
+//   storageBucket: "fwan-98510.firebasestorage.app",
+//   messagingSenderId: "789913628663",
+//   appId: "1:789913628663:web:63fd7cda966680fe0448f2",
+//   measurementId: "G-TNJRXCK735",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app); // ✅ Export Auth for use in your app
+// export const db = getFirestore(app); // ✅
+// getAnalytics(app);
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "fwan-98510.firebaseapp.com",
@@ -16,6 +40,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app); // ✅ Export Auth for use in your app
-export const db = getFirestore(app); // ✅
-getAnalytics(app);
+const analytics = getAnalytics(app);
